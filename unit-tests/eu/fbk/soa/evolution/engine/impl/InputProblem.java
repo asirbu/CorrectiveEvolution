d308c4d08354de8f166f0ce1f6f4ef0dd54e290e
Adina Sirbu <mail@adinasirbu.net>
2013-06-14 03:28:17 +0200

    Added a round-trip test (STS to SMV and back to STS)

---
+++ unit-tests/eu/fbk/soa/evolution/engine/impl/InputProblem.java
@@ -0,0 +1,146 @@
+package eu.fbk.soa.evolution.engine.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+
+import eu.fbk.soa.evolution.Correction;
+import eu.fbk.soa.evolution.Correction.Type;
+import eu.fbk.soa.process.Activity;
+import eu.fbk.soa.process.Adaptation;
+import eu.fbk.soa.process.DefaultProcessModel;
+import eu.fbk.soa.process.Effect;
+import eu.fbk.soa.process.ProcessModel;
+import eu.fbk.soa.process.StateFormula;
+import eu.fbk.soa.process.Trace;
+import eu.fbk.soa.process.domain.DomainObject;
+import eu.fbk.soa.process.domain.ObjectEvent;
+import eu.fbk.soa.process.domain.ObjectState;
+import eu.fbk.soa.process.domain.ObjectTransition;
+import eu.fbk.soa.process.domain.StateLiteral;
+import eu.fbk.soa.process.node.ActivityNode;
+import eu.fbk.soa.process.node.ProcessNode;
+import eu.fbk.soa.process.node.StartNode;
+import eu.fbk.soa.process.node.XorJoin;
+import eu.fbk.soa.process.node.XorSplit;
+
+public class InputProblem {
+
+	private ProcessModel model;
+
+	private Correction correction;
+	
+	private Set<Condition> conds;
+
+	private List<Activity> activities;
+
+	public Correction getCorrection() {
+		return correction;
+	}
+
+	public Set<Condition> getConditions() {
+		return conds;
+	}
+
+	private List<ProcessNode> actList;
+
+	private ProblemToSTS pb2sts;
+	
+	
+	public InputProblem() {
+		model = createProcessModel();
+
+		List<Correction> corrections = new ArrayList<Correction>();
+		correction = this.createCorrection();
+		corrections.add(correction);
+
+		conds = new HashSet<Condition>();
+		conds.add(new Condition(correction.getCondition(), 1));
+		pb2sts = new ProblemToSTS(model, corrections, conds);
+	}
+
+	/*
+	 * Creating the process:
+	 * A0 -> XorSplit -> A1 -> A2 -> XorJoin -> A4
+	 * 				  -> A3		  ->		
+	 */
+	private ProcessModel createProcessModel() {
+		this.activities = new ArrayList<Activity>();
+
+		Set<ProcessNode> nodes = new HashSet<ProcessNode>();
+		actList = new ArrayList<ProcessNode>();
+		StartNode start = null;
+
+		for (int i = 0; i <= 4; i++) {
+			Activity ai = new Activity("A" + i, new StateFormula(), new Effect());
+			activities.add(ai);
+			ProcessNode node;
+			if (i == 0) {
+				start = new StartNode(ai);
+				node = start;
+			} else {
+				node = new ActivityNode(ai);
+			}
+			actList.add(node);
+			nodes.add(node);
+		}
+		
+		XorSplit xorsplit = new XorSplit();
+		nodes.add(xorsplit);
+		
+		XorJoin xorjoin = new XorJoin();
+		nodes.add(xorjoin);
+		
+		ProcessModel model = new DefaultProcessModel("TestProcess", nodes);
+
+		model.addEdge(start, xorsplit);
+		model.addEdge(xorsplit, actList.get(1));
+		model.addEdge(xorsplit, actList.get(3));
+		model.addEdge(actList.get(1), actList.get(2));
+		model.addEdge(actList.get(2), xorjoin);
+		model.addEdge(actList.get(3), xorjoin);		
+		model.addEdge(xorjoin, actList.get(4));
+		return model;
+	}
+
+
+	private Correction createCorrection() {
+		
+		ObjectState state1 = new ObjectState("State1");
+		Set<ObjectState> states = new HashSet<ObjectState>();
+		states.add(state1);
+		DomainObject obj = new DomainObject("Obj", states,
+				new HashSet<ObjectState>(), new HashSet<ObjectEvent>(),
+				new HashSet<ObjectTransition>());
+		
+		StateFormula cond = new StateFormula(new StateLiteral(obj, state1));
+
+		int fromActIndex = 1;
+		List<Activity> executedActs = new ArrayList<Activity>();
+		for (int i = 0; i <= fromActIndex; i++) {
+			executedActs.add(activities.get(i));
+		}
+		Trace trace = new Trace(executedActs);
+
+		ActivityNode fromNode = (ActivityNode) actList.get(fromActIndex);
+		ActivityNode toNode = (ActivityNode) actList.get(fromActIndex + 1);
+
+		Set<ProcessNode> adaptNodes = new HashSet<ProcessNode>();
+		adaptNodes.add(new ActivityNode("Fix"));
+		ProcessModel adaptPM = new DefaultProcessModel("AdaptationModel", adaptNodes);
+		Adaptation adaptation = new Adaptation(adaptPM, fromNode, toNode);
+
+		Correction correction = new Correction(Type.STRICT, trace, cond, adaptation);
+		return correction;
+	}
+	
+	public ProcessModel getProcessModel() {
+		return this.model;
+	}
+	
+}
---
+++ unit-tests/eu/fbk/soa/evolution/engine/impl/ProblemToSMVTest.java
@@ -0,0 +1,194 @@
+package eu.fbk.soa.evolution.engine.impl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.fbk.soa.evolution.Correction;
+import eu.fbk.soa.evolution.Correction.Type;
+import eu.fbk.soa.evolution.sts.Action;
+import eu.fbk.soa.evolution.sts.STS;
+import eu.fbk.soa.evolution.sts.State;
+import eu.fbk.soa.evolution.sts.Transition;
+import eu.fbk.soa.evolution.sts.nusmv.PathExplorer;
+import eu.fbk.soa.process.Activity;
+import eu.fbk.soa.process.Adaptation;
+import eu.fbk.soa.process.DefaultProcessModel;
+import eu.fbk.soa.process.Effect;
+import eu.fbk.soa.process.ProcessModel;
+import eu.fbk.soa.process.StateFormula;
+import eu.fbk.soa.process.Trace;
+import eu.fbk.soa.process.domain.DomainObject;
+import eu.fbk.soa.process.domain.ObjectState;
+import eu.fbk.soa.process.domain.StateLiteral;
+import eu.fbk.soa.process.node.ActivityNode;
+import eu.fbk.soa.process.node.ProcessNode;
+import eu.fbk.soa.process.node.StartNode;
+import eu.fbk.soa.process.node.XorJoin;
+import eu.fbk.soa.process.node.XorSplit;
+import eu.fbk.soa.util.ConfigUtils;
+import eu.fbk.soa.util.IOUtils;
+
+public class ProblemToSMVTest {
+
+	private InputProblem problem;
+	
+	private ProcessModel model;
+	
+	private List<Correction> corrections;
+	
+	private ProblemToSMV pb2smv;
+	
+	@Before
+	public void setUpProblem() {
+		PropertyConfigurator.configure("log4j.properties");
+		problem = new InputProblem();
+		
+		model = problem.getProcessModel();
+		
+		corrections = new ArrayList<Correction>();
+		corrections.add(problem.getCorrection());
+	
+	}
+	
+	
+	@Test
+	public void testModules() {
+		pb2smv = new ProblemToSMV(model, corrections);
+		String smvStr = pb2smv.translateProblemToSMV();
+		String[] tokens = smvStr.split("MODULE");
+		int nrOfModules = tokens.length - 1;
+				
+		assertEquals("There should be 6 separate modules in the smv text", 6, nrOfModules);
+	}
+	
+	@Test
+	public void rountripTest() throws IOException {
+		ProblemToSTS pb2sts = new ProblemToSTS(model,  new ArrayList<Correction>(), 
+				new HashSet<Condition>());
+		pb2sts.translateProblem2STS();
+		STS modelSTS = pb2sts.getProcessModelSTS(model);
+		
+		String outDirPath = ConfigUtils.getProperty("outputDir");
+		File outDir = new File(outDirPath);
+		if (!outDir.exists()) {
+			outDir.mkdirs();
+		}
+		
+		pb2smv = new ProblemToSMV(model, new ArrayList<Correction>());
+		String smvStr = pb2smv.translateProblemToSMV();
+		String smvFileName = outDirPath + File.separator + "simple_pb.smv";
+		IOUtils.writeStringToFile(smvStr, smvFileName);
+
+		PathExplorer explorer = new PathExplorer(outDirPath + File.separator);
+		STS stsAfterEncoding = explorer.explore(smvFileName);
+		
+		File intermediaryFile = new File(smvFileName);
+		intermediaryFile.delete();
+		
+		assertTrue("Resulting STSs should only differ in state names and XorSplit actions",
+				similarExceptForStateNames(modelSTS, stsAfterEncoding));
+	}
+
+
+	private boolean similarExceptForStateNames(STS sts1, STS sts2) {
+		Map<Action, Action> correspAction = new HashMap<Action, Action>();
+		Map<Transition, Transition> correspTransition = new HashMap<Transition, Transition>();
+		Map<State, State> correspState = new HashMap<State, State>();
+		
+		boolean inputsMatch = matchInputActions(sts1, sts2, correspAction);
+		if (!inputsMatch) {
+			return false;
+		}
+		
+		boolean transMatch = matchTransitions(sts1, sts2, correspAction, correspTransition);
+		if (!transMatch) {
+			return false;
+		}
+		
+		State init1 = sts1.getInitialState();
+		correspState.put(init1, sts2.getInitialState());
+		
+		for (Transition t1 : correspTransition.keySet()) {
+			Transition t2 = correspTransition.get(t1);
+			
+			State corr = correspState.get(t1.getSource());
+			if (corr != null && !corr.equals(t2.getSource())) {
+				return false;
+			}
+			correspState.put(t1.getSource(), t2.getSource());
+			
+			State corrTarget = correspState.get(t1.getTarget());
+			if (corrTarget != null && !corrTarget.equals(t2.getTarget())) {
+				return false;
+			}
+			correspState.put(t1.getSource(), t2.getSource());
+		}
+		
+		return true;
+	}
+	
+	private boolean matchTransitions(STS sts1, STS sts2,
+			Map<Action, Action> correspAction,
+			Map<Transition, Transition> correspTransition) {
+		
+		for (Transition t1 : sts1.getTransitions()) {
+			Action a2 = correspAction.get(t1.getAction());
+			if (a2 != null) {
+				boolean foundTrans = false;
+				for (Transition t2 : sts2.getTransitions()) {
+					if (t2.getAction().equals(a2)) {
+						correspTransition.put(t1, t2);
+						foundTrans = true;
+					}
+				}
+				if (!foundTrans) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+
+	private boolean matchInputActions(STS sts1, STS sts2, Map<Action, Action> correspAction) {
+		if (sts1.getInputActions().size() != sts2.getInputActions().size()) {
+			return false;
+		}
+		for (Action act1 : sts1.getInputActions()) {
+			String actName = act1.getName();
+			if (!actName.contains("Xor")) { 
+				Action act2 = sts2.getAction(actName);
+				if (act2 == null) {
+					return false;
+				}
+				correspAction.put(act1, act2);
+			} else {
+				boolean foundXor = false;
+				for (Action act2 : sts2.getInputActions()) {
+					if (act2.getName().contains("Xor")) {
+						correspAction.put(act1, act2);
+						foundXor = true;
+						break;
+					}
+				}
+				if (!foundXor) {
+					return false;
+				}
+			}	
+		}
+		return true;
+	}
+
+}
